108dd08fb28b0f7fa1b05d983b637ca1559fac0d
Carlj0666 <carlj1881@yahoo.com>
2022-03-22 18:44:47 -0700

    debugging move controls

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const grid = document.querySelector('.grid');
     const points = document.getElementById('points');
+    const grid = document.querySelector('.grid');
     const width = 28;
 
     //Layout key:
@@ -48,11 +48,11 @@ function createMap() {
     //loop over the entire length of the layout
     for (let i = 0; i < layout.length; i++) {
     //create a div for each element
-        const square = document.createElement('div')
+        const square = document.createElement('div');
     //add the divs - (squares) to the grid
-        grid.appendChild(square)
+        grid.appendChild(square);
     //add the dives to a new array
-        squares.push(square)
+        squares.push(square);
     //Use the layout to populate the board with the css styling
         //Layout key:
         // 0 - .dots
@@ -61,13 +61,13 @@ function createMap() {
         // 3 - .powerpill
         // 4 - .empty
         if (layout[i] === 0) {
-            squares[i].classList.add('dots')
+            squares[i].classList.add('dots');
         } else if (layout[i] === 1) {
-            squares[i].classList.add('wall')
+            squares[i].classList.add('wall');
         } else if (layout[i] === 2) {
-            squares[i].classList.add('ghost-house')
+            squares[i].classList.add('ghost-house');
         } else if (layout[i] === 3) {
-            squares[i].classList.add('powerpill')
+            squares[i].classList.add('powerpill');
         }
     }
 }
@@ -80,6 +80,36 @@ let pillConsumerCurrentIndex = 489;
 //Add the css styling to the array position index
 squares[pillConsumerCurrentIndex].classList.add('pill-consumer');
 
+//Movement
+function movePillConsumer(e) {
+    //remove PCCI from the current grid section
+    squares[pillConsumerCurrentIndex].remove('pill-consumer');
+    //use a switch to decide which new index to move to
+    console.log(e.currentTarget.keycode)
+    switch(e.currentTarget.keyCode) {
+        
+        case 37:
+            if (pillConsumerCurrentIndex % width !== 0) pillConsumerCurrentIndex -= 1;
+            break;
+        case 38:
+            if (pillConsumerCurrentIndex - width >= 0) pillConsumerCurrentIndex -= width;
+            break;
+        case 39:
+            if (pillConsumerCurrentIndex % width < width -1) pillConsumerCurrentIndex += 1;
+            break;
+        case 40:
+            if (pillConsumerCurrentIndex + width < width * width) pillConsumerCurrentIndex += width;
+            break;
+    }
+    squares[pillConsumerCurrentIndex].classList.add('pill-consumer');
 
+    //pillKill()
+    //pillConsume()
+    //checkGameOver()
+    //checkGameWin()
+    }
+    
+    document.addEventListener('keyup', movePillConsumer)
+    console.log()
 });
 
